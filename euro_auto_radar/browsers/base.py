5dd079995afe88665904768a0784980d82beb39a
IGOR COSTA DO COUTO <igor.couto@trt8.jus.br>
2025-05-01 02:27:30 +0100

    Add browser automation for make/model selection and improve element handling

--- euro_auto_radar/browsers/base.py
+++ euro_auto_radar/browsers/base.py
@@ -1,21 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from typing import Optional
 
 class BaseBrowser:
-    def __init__(self, headless: bool = True):
-        self.driver: Optional[WebDriver] = None
-        self.headless = headless
-
-    def create_driver(self):
-        options = Options()
-        if self.headless:
-            options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        self.driver = webdriver.Chrome(options=options)
+    def __init__(self, headless: bool = True, default_timeout: int = 10):
+        self.driver = self._create_driver(headless)
+        self.wait = WebDriverWait(self.driver, default_timeout)
 
     def close_driver(self):
         if self.driver:
@@ -23,9 +16,38 @@ class BaseBrowser:
             self.driver = None
 
     def navigate(self, url: str):
-        if not self.driver:
-            self.create_driver()
         self.driver.get(url)
+
+    def _create_driver(self, headless):
+        options = Options()
+        if headless:
+            options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        return webdriver.Chrome(options=options)
+    def _element_in_viewport(self, locator):
+        element = self.driver.find_element(*locator)
+        return self.driver.execute_script("""
+            var elem = arguments[0],
+                box = elem.getBoundingClientRect(),
+                cx = box.left + box.width / 2,
+                cy = box.top + box.height / 2,
+                e = document.elementFromPoint(cx, cy);
+            for (; e; e = e.parentElement) {
+                if (e === elem)
+                    return true;
+            }
+            return false;
+        """, element)
+
+    def _get_and_move_to_element(self, locator, expected_condition=EC.presence_of_element_located, parent_element=None):
+        search_context = parent_element if parent_element is not None else self.driver
+        def find_in_context(driver):
+            return expected_condition(locator)(search_context)
+        element = self.wait.until(find_in_context)
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+        self.wait.until(lambda d: self._element_in_viewport(locator) if parent_element is None else self._element_in_viewport_within(locator, parent_element))
+        return element
 
     def __enter__(self):
         self.create_driver()
--- euro_auto_radar/browsers/standvirtual.py
+++ euro_auto_radar/browsers/standvirtual.py
@@ -2,9 +2,44 @@ from .base import BaseBrowser
 from euro_auto_radar.models.car import Car
 from euro_auto_radar.models.car_filter import CarFilter
 from typing import List
+from selenium.webdriver.common.by import By
 
 class StandVirtualBrowser(BaseBrowser):
     BASE_URL = "https://www.standvirtual.com/carros"
 
     def search(self, car_filter: CarFilter) -> List[Car]:
+        self.navigate(self.BASE_URL)
+        self.accept_cookies()
+        if car_filter.make:
+            self.select_make(car_filter.make)
+            if car_filter.model:
+                self.select_model(car_filter.model)
+
+        pass
+
+    def accept_cookies(self):
+        accept_button = self._get_and_move_to_element((By.ID, 'onetrust-accept-btn-handler'))
+        accept_button.click()
+
+    def select_make(self, make_name):
+        make_filter_locator = (By.XPATH, '//*[@data-testid="filter_enum_make"]')
+        self._get_and_move_to_element(make_filter_locator).click()
+        make_filter_el = self._get_and_move_to_element(make_filter_locator)
+        for p in make_filter_el.find_elements(By.XPATH, './/p'):
+            if make_name.lower() in p.text.lower():
+                p.click()
+                break
+        make_arrow_el = make_filter_el.find_element(By.XPATH, './/*[@data-testid="arrow"]')
+        make_arrow_el.click()
+
+    def select_model(self, model_name):
+        model_filter_locator = (By.XPATH, '//*[@data-testid="filter_enum_model"]')
+        self._get_and_move_to_element(model_filter_locator).click()
+        model_filter_el = self._get_and_move_to_element(model_filter_locator)
+        for p in model_filter_el.find_elements(By.XPATH, './/p'):
+            if model_name.lower() in p.text.lower():
+                p.click()
+                break
+        model_arrow_el = model_filter_el.find_element(By.XPATH, './/*[@data-testid="arrow"]')
+        model_arrow_el.click()
         pass
